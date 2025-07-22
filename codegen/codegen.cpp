86b4d1a266e762584d057c47f3f51b20930d84f3
1054013972@qq.com <1054013972@qq.com>
2025-07-22 22:14:36 +0800

    modify transfer params

--- codegen/codegen.cpp
+++ codegen/codegen.cpp
@@ -810,7 +810,15 @@ void CodeGenerator::processCall(const std::shared_ptr<CallInstr>& instr) {
 
     // 6.2 栈参数
     int stackParamOffset = callerRegsSize + calleeRegsSize; // 栈参数起始偏移
-    for (int i = 8; i < paramCount; ++i) {
+    /*for (int i = 8; i < paramCount; ++i) {
+        if (!params[i]) continue;
+        std::string tempReg = allocTempReg();
+        loadOperand(params[i], tempReg);
+        emitInstruction("sw " + tempReg + ", " + std::to_string(stackParamOffset) + "(sp)");
+        stackParamOffset += 4;
+        freeTempReg(tempReg);
+    }*/
+    for (int i = paramCount - 1; i >= 8; --i) {
         if (!params[i]) continue;
         std::string tempReg = allocTempReg();
         loadOperand(params[i], tempReg);
