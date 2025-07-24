55fccdabf01c2f8319a425796069821e532fdee2
1054013972@qq.com <1054013972@qq.com>
2025-07-24 16:42:22 +0800

    control flow

--- ir/irgen.cpp
+++ ir/irgen.cpp
@@ -414,7 +414,7 @@ void IRGenerator::optimize() {
     constantFolding();        // 在编译时评估常量表达式
     //constantPropagation();    // 在代码中传播常量值
     deadCodeElimination();    // 删除无效果的代码
-    //controlFlowOptimization(); // 优化控制流（跳转、分支等）
+    controlFlowOptimization(); // 优化控制流（跳转、分支等）
 }
 
 /**
