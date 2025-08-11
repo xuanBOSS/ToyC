f3b46140ba6a1054359b5032b35e44880ebcd395
1054013972@qq.com <1054013972@qq.com>
2025-08-11 15:55:25 +0800

    test step1

--- ir/irgen.cpp
+++ ir/irgen.cpp
@@ -1775,7 +1775,7 @@ bool IRGenerator::validateCFG(const std::vector<std::shared_ptr<BasicBlock>>& bl
 
 void IRGenerator::controlFlowOptimization() {
     // 构建基本块和CFG
-    /*auto blocks = buildBasicBlocks();
+    auto blocks = buildBasicBlocks();
     buildCFG(blocks);
     if (blocks.empty()) return;
 
@@ -1798,7 +1798,7 @@ void IRGenerator::controlFlowOptimization() {
     blocks.swap(newBlocks);
 
     // Step 2: 合并直连基本块
-    std::unordered_set<std::shared_ptr<BasicBlock>> toRemove;
+    /*std::unordered_set<std::shared_ptr<BasicBlock>> toRemove;
 
     for (auto& blk : blocks) {
         if (blk->instructions.empty()) continue;
