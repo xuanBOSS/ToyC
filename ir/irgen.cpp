a3d73ce3051972bca02929828667a63d1679e231
1054013972@qq.com <1054013972@qq.com>
2025-08-13 11:02:19 +0800

    copy

--- ir/irgen.cpp
+++ ir/irgen.cpp
@@ -413,7 +413,7 @@ void IRGenerator::optimize() {
     // 按顺序应用每种优化技术
     constantFolding();        // 在编译时评估常量表达式
     constantPropagationCFG();    // 在代码中传播常量值
-    //copyPropagationCFG();       // 复制传播优化
+    copyPropagationCFG();       // 复制传播优化
     deadCodeElimination();    // 删除无效果的代码
     //controlFlowOptimization(); // 优化控制流（跳转、分支等）
 }
